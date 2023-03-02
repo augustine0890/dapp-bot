143665156f8265898ff51456ca704c8e1ca9c85b
augustine <augustino0890@gmail.com>
2023-03-02 17:30:23 +0900

    add activity model

--- internal/database/model.go
+++ internal/database/model.go
@@ -10,3 +10,15 @@ type User struct {
 	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
 	UpdatedAt  time.Time `bson:"updatedAt" json:"updatedAt"`
 }
+
+type Activity struct {
+	User      string    `json:"user" bson:"user" required:"true"`
+	UserName  string    `json:"userName" bson:"userName"`
+	ChannelId string    `json:"channelId" bson:"channelId" required:"true"`
+	Activity  string    `json:"activity" bson:"activity" required:"true" enum:"attend,react,receive,play"`
+	Reward    int       `json:"reward" bson:"reward" required:"true" enum:"-10,5,10,50"`
+	MessageId string    `json:"messageId" bson:"messageId"`
+	Emoji     string    `json:"emoji" bson:"emoji"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+}
